845d944801071c46d9d787b5961f679825771d1f
Abdulrahman-19 <abedabogneem@gmail.com>
2021-03-15 14:08:03 +0200

    About-me

---
+++ JS/app.js
@@ -0,0 +1,59 @@
+'use strict';
+
+let userName = prompt('Enter your Name');
+alert('welcome '+ userName);
+
+alert('answer by typing yes/no');
+let x = prompt('is my name start with AB letters?');
+
+if (x.toLocaleLowerCase() === 'yes'){
+  //console.log('correct');
+  alert('correct');
+}
+else{
+  //console.log('incorrect');
+  alert('incorrect');
+}
+
+let y = prompt('do I live in irbid?');
+
+if (y.toLocaleLowerCase() === 'yes'){
+  //console.log('correct');
+  alert('correct');
+}
+else{
+  //console.log('incorrect');
+  alert('incorrect');
+}
+let g = prompt('did I studed computer engineering in AHU?');
+
+if (g.toLocaleLowerCase() === 'yes'){
+  //console.log('correct');
+  alert('correct');
+}
+else{
+  //console.log('incorrect');
+  alert('incorrect');
+}
+let a = prompt('Am I bold?');
+
+if (a.toLocaleLowerCase() === 'no'){
+  //console.log('correct');
+  alert('correct');
+}
+else{
+  //console.log('incorrect');
+  alert('incorrect');
+}
+
+let b = prompt('Am I 27 years old?');
+
+if (b.toLocaleLowerCase() === 'yes'){
+  //console.log('correct');
+  alert('correct');
+}
+else{
+  //console.log('incorrect');
+  alert('incorrect');
+}
+
