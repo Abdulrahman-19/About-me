f28877e18b82181929ceabef2fbc2c19de249fda
Abdulrahman-19 <abedabogneem@gmail.com>
2021-03-17 17:26:36 +0200

    class

--- JS/app.js
+++ JS/app.js
@@ -8,59 +8,59 @@ let x = prompt('is my name start with AB letters?');
 let l = 0;
 
 if (x.toLocaleLowerCase() === 'yes'){
-  //console.log('correct');
-  alert('correct');
+  //console.log('b ');
+  alert('b ');
   l=l+1;
 }
 else{
-  //console.log('incorrect');
-  alert('incorrect');
+  //console.log('inb ');
+  alert('inb ');
 }
 
 let y = prompt('do I live in irbid?');
 
 if (y.toLocaleLowerCase() === 'yes'){
-  //console.log('correct');
-  alert('correct');
+  //console.log('b ');
+  alert('b ');
   l=l+1;
 }
 else{
-  //console.log('incorrect');
-  alert('incorrect');
+  //console.log('inb ');
+  alert('inb ');
 }
 let g = prompt('did I studed computer engineering in AHU?');
 
 if (g.toLocaleLowerCase() === 'yes'){
-  //console.log('correct');
-  alert('correct');
+  //console.log('b ');
+  alert('b ');
   l=l+1;
 }
 else{
-  //console.log('incorrect');
-  alert('incorrect');
+  //console.log('inb ');
+  alert('inb ');
 }
 let a = prompt('Am I bold?');
 
 if (a.toLocaleLowerCase() === 'no'){
-  //console.log('correct');
-  alert('correct');
+  //console.log('b ');
+  alert('b ');
   l=l+1;
 }
 else{
-  //console.log('incorrect');
-  alert('incorrect');
+  //console.log('inb ');
+  alert('inb ');
 }
 
 let b = prompt('Am I 27 years old?');
 
 if (b.toLocaleLowerCase() === 'yes'){
-  //console.log('correct');
-  alert('correct');
+  //console.log('b ');
+  alert('b ');
   l=l+1;
 }
 else{
-  //console.log('incorrect');
-  alert('incorrect');
+  //console.log('inb ');
+  alert('inb ');
 }
 
 for(let i = 0; i<4 ;i++)
@@ -68,7 +68,7 @@ for(let i = 0; i<4 ;i++)
   let number = parseInt(prompt('gues a number between 0 and 20?'));
   if (number === 10 )
   {
-    alert('correct');
+    alert('b ');
     l=l+1;
     break;
   }
@@ -88,36 +88,25 @@ for(let i = 0; i<4 ;i++)
 alert('the number is 10');
 
 let s =['red','blue','black','green'] ;
-for(let a = 0; a < 6 ; a++)
-{
-  let arr = prompt('what is my favorit colors? (4 colors)');
-  if(arr===s[0]){
-    alert('correct');
-    l=l+1;
-    break;
-  }
-  else if(arr===s[1]){
-    alert('correct');
-    l=l+1;
-    break;
-  }
-  else if(arr===s[2]){
-    alert('correct');
-    l=l+1;
-    break;
-  }
-  else if(arr===s[3]){
-    alert('correct');
-    l=l+1;
-    break;
-  }
-  else {
-    alert('incorrect');
+let p = false;
+for (let z=0; z<6 ;z++){
+  for(let a = 0; a < s.length ; a++)
+  {
+    let arr = prompt('what is my favorit colors? (4 colors)');
+    if(arr===s[a]){
+      p = true;
+      break;
+    }
+    if (p){
+      alert('correct');
+      l=l+1;
+      break;
+    }
+    else {
+      alert('incorrect');
+    }
   }
 }
-alert('the colors are: '+ s[0]);
-alert('the colors are: '+ s[1]);
-alert('the colors are: '+ s[2]);
-alert('the colors are: '+ s[3]);
-alert('your score is : '+ l);
+alert('the colors are: '+ s);
+
 
